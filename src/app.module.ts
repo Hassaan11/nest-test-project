6828102caebeef7de0bb5604c2f1fbe4da6531a3
Hassaan11 <saad117218@gmail.com>
2023-06-01 15:33:23 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],
+})
+export class AppModule {}
+
+// app.module is the main module of app that will imort other modules it is similar to app.js in react
+// nest g module user to autogenerate module of user
